ec8f611cd35d8f92d3cbef4d3b4a59b9ed8677dc
Greg Statton <greg@nutanix.com>
2013-08-19 10:21:10 -0700

    adding data formating api for Pearson Corellation algorythm

--- server.js
+++ server.js
@@ -121,15 +121,111 @@ app.get(baseurl + 'shortcode', function (req, res){
   })
 })
 
+//GET Nested Dictonary for Pearson Correlation
+app.get(baseurl + 'pearson', function (req, res){
+	results = "results";
+	var pearsondict = [results];
+	var newentry = {};
+	db.links.find({}, function(err, data){
+		returnlen = data.length
+		for (var i = 0; i < returnlen; i++){
+			data = lookuper(data[i].url);
+			pearsondict.push(data);
+			console.log(pearsondict);
+		};
+	res.json(JSON.stringify(pearsondict));
+	});
+	//console.log("3" + JSON.stringify(pearsondict));
+
+})
+
 //PUTS
 app.put(baseurl + 'overit', function (req, res){
 	//post a new OverIt
-	//Params: Twitter Userid (username)
+	//Params: TwitterName retrieved via Loged-In user
 	//        URL (url)
-	username = req.query.username;
-	url = req.query.url;
+	var url = req.query.url;
+	var shortcode = makeid(url);
+    var datetime = new Date().getTime();
+
+	db.overits.save({timestame: datetime , user: req.user.twitter.screen_name, url: url}, function(err, overits){
+    console.log("saving the overit...");
+      
+      // If the URL doesn't start with http, add it.
+      if (url.search(/^http/) == -1) {
+          url = 'http://' + url;
+      
+      }
+
+      console.log("here's the shortcode: " + shortcode);
+      
+
+          db.links.save({shortcode: shortcode, url: url}, function(err, saved) {
+
+            if( err || !saved ) {
+               console.log("Link not saved...already exists");
+               db.links.find({url: url}, function(err, data){
+                  console.log("here's the shortcode: " + JSON.stringify(data[0].shortcode));
+
+                 // res.redirect('/' + data[0].shortcode);
+				  res.json([{"status": 201, "shortcode": shortcode}])
+               });
+
+              //res.send("Here's your shortcode: " + shortcode);
+            } else {
+               console.log("Link saved: http://www.delike.us/" + shortcode );
+              //res.redirect('/'+shortcode);
+				res.json([{"status": 201, "shortcode": shortcode }])
+            }
+
+          });
+      })
 	
 })
+
+//Look-up user count for a given link
+function lookuper(url)
+{
+	//console.log(data[i].url);
+	newurl = url;
+	obj1 = [];
+	obj2 = [];
+	db.overits.aggregate( [ { $match: {url: newurl} }, 
+							{ $group: { _id: "$user", total: { $sum: 1 }, url: { $addToSet: "$url"}, user: { $addToSet: "$user" }  } }, 
+							{ $sort: { total: -1 } } ], function (err, results){
+			for (var u = 0; u < results[0].user.length; u++){
+				nexturl = JSON.stringify(results[0].url[0]);
+				nextuser = JSON.stringify(results[0].user[u]);
+				nexttotal = JSON.stringify(results[0].total);
+				user = JSON.stringify(results.user);
+				//console.log("url: " + nexturl + "user: " + nextuser + " total: " + nexttotal);
+				//Build JSON object that looks like {"http://www.google.com": {"gstatton": 3, "mrkai": 5, "jameschho": 1}}
+				obj1 = [nexturl];
+				obj2 = [nextuser];
+				obj2.push(nexttotal);
+				data = {user : nexttotal};
+				obj1.push(obj2);
+				//newobj.userobj.push(nexttotal);
+				//console.log(JSON.stringify(tempobj));
+			}
+			//console.log(obj1);
+			return obj1;
+		});
+	//console.log(obj1);	
+	//return obj1;
+}
+
+// Link shortener logic
+function makeid()
+{
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for( var i=0; i < 5; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
 
 
 
